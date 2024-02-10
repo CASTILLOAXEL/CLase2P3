970648d8240ba1bd51393679cedf1ad974cdbb00
AXEL CASTILLO <acastilloy2@miumg.edu.gt>
2024-02-10 08:58:29 -0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.example.clase.Persona;
+import org.example.clase.TablaPronostico;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TablaPronostico tabla = new TablaPronostico();
+        tabla.calculos();
+
+
+
+//        Persona[][] matrix = new Persona[2][2];
+//        matrix[0][0]= new Persona("juan",20);
+//        matrix[0][1]= new Persona("Maria",26);
+//        matrix[1][0]= new Persona("Pedro",29);
+//        matrix[1][1]= new Persona("Carlos",21);
+//
+//        for (Persona[]fila: matrix){
+//            for (Persona persona: fila){
+//                System.out.println(persona);
+//            }
+//        }
+
+//     Persona[] personas = new Persona[3];
+//
+//     //ASIGNAR EL OBJETO PERSONA
+//
+//        personas[0]= new Persona("Juan", 20);
+//        personas[1] =new Persona("Maria", 26);
+//        personas[2] = new Persona("Pedro", 29);
+//
+//        for (Persona persona: personas);
+//        {
+//            System.out.println(persona);
+//        }
+//
+
+
+
+    }
+}
